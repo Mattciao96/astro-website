87c1e0ed6a20b841421a1822313523c34cd2a877
Matteo Conti <matt.ciao@gmail.com>
2024-06-07 16:59:32 +0200

    add shiki markdown config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,20 @@
 import { defineConfig } from 'astro/config';
-
 import tailwind from "@astrojs/tailwind";
+
+const markdownConfigs = {
+  shikiConfig: {
+    themes: {
+      light: 'catppuccin-latte',
+      dark: 'catppuccin-macchiato'
+    },
+    langs: [],
+    wrap: true,
+    transformers: []
+  }
+};
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()]
+  integrations: [tailwind()],
+  markdown: markdownConfigs
 });
