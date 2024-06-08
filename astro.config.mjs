adede69788337aa8c156d6f93b2ce43610836bc4
Matteo Conti <matt.ciao@gmail.com>
2024-06-08 17:22:43 +0200

    add react

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,25 +1,27 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
-
+import react from "@astrojs/react";
 const markdownConfig = {
   shikiConfig: {
     themes: {
       light: "catppuccin-latte",
-      dark: "catppuccin-macchiato",
+      dark: "catppuccin-macchiato"
     },
     langs: [],
     wrap: true,
-    transformers: [],
-  },
+    transformers: []
+  }
+};
+const imageConfig = {
+  remotePatterns: [{
+    protocol: "https"
+  }]
 };
 
-const imageConfig = {
-  remotePatterns: [{ protocol: "https" }],
-};
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()],
+  integrations: [tailwind(), react()],
   markdown: markdownConfig,
-  image: imageConfig,
+  image: imageConfig
 });
